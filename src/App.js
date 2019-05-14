ca6cc6e898ef08c2bacd036bb16f1cc654a16cb9
sumanthkumarbkr <sumanthkumarbkr@gmail.com>
2019-05-14 22:30:09 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Navbar,NavbarBrand } from 'reactstrap';
+import Menu from './componets/MenuComponent'; 
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
+class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      dishes:DISHES
+    };
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorent con fussion</NavbarBrand>
+          </div>
+      </Navbar>
+      <Menu dishes={this.state.dishes} />
     </div>
   );
+}
 }
 
 export default App;
